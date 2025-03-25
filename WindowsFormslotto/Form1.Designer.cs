39a735cea3cf0dcf59164f1d52ddc1ab8bba92cb
mohanadabouzaidi <mohanad.abouzaidi@gmail.com>
2025-03-25 21:28:59 +0100

    Add project files.

---
+++ WindowsFormslotto/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormslotto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.l_chiffreChoisis = new System.Windows.Forms.Label();
+            this.l_chiffreGagnants = new System.Windows.Forms.Label();
+            this.l_listeDesChiffresGagnant = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(62, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(580, 569);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 625);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chiffre Choisis: ";
+            // 
+            // l_chiffreChoisis
+            // 
+            this.l_chiffreChoisis.AutoSize = true;
+            this.l_chiffreChoisis.Location = new System.Drawing.Point(183, 625);
+            this.l_chiffreChoisis.Name = "l_chiffreChoisis";
+            this.l_chiffreChoisis.Size = new System.Drawing.Size(0, 20);
+            this.l_chiffreChoisis.TabIndex = 2;
+            // 
+            // l_chiffreGagnants
+            // 
+            this.l_chiffreGagnants.AutoSize = true;
+            this.l_chiffreGagnants.Location = new System.Drawing.Point(58, 678);
+            this.l_chiffreGagnants.Name = "l_chiffreGagnants";
+            this.l_chiffreGagnants.Size = new System.Drawing.Size(51, 20);
+            this.l_chiffreGagnants.TabIndex = 3;
+            this.l_chiffreGagnants.Text = "label2";
+            // 
+            // l_listeDesChiffresGagnant
+            // 
+            this.l_listeDesChiffresGagnant.AutoSize = true;
+            this.l_listeDesChiffresGagnant.Location = new System.Drawing.Point(197, 678);
+            this.l_listeDesChiffresGagnant.Name = "l_listeDesChiffresGagnant";
+            this.l_listeDesChiffresGagnant.Size = new System.Drawing.Size(51, 20);
+            this.l_listeDesChiffresGagnant.TabIndex = 4;
+            this.l_listeDesChiffresGagnant.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 775);
+            this.Controls.Add(this.l_listeDesChiffresGagnant);
+            this.Controls.Add(this.l_chiffreGagnants);
+            this.Controls.Add(this.l_chiffreChoisis);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label l_chiffreChoisis;
+        private System.Windows.Forms.Label l_chiffreGagnants;
+        private System.Windows.Forms.Label l_listeDesChiffresGagnant;
+    }
+}
+
---
+++ WindowsFormslotto/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Auteur : Cyril Vandervelde
+// Auteur : Cyril Vandervelde
+namespace WindowsFormslotto
+{
+    public partial class Form1 : Form
+    {
+        List<Label> selectedLabels = new List<Label>();
+        int maxSelectedLabels = 7; // Changé de 4 à 7
+        List<int> chiffresGagnants = new List<int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitiData();
+            DisplayNumbersInPanel();
+
+        }
+
+        private void InitiData()
+        {
+            label1.Hide();
+            l_chiffreChoisis.Hide();
+            l_chiffreGagnants.Hide();
+            l_listeDesChiffresGagnant.Hide();
+            label1.Text = "";
+            l_chiffreChoisis.Text = "";
+            l_chiffreGagnants.Text = "";
+            l_listeDesChiffresGagnant.Text = "";
+        }
+
+        private void DisplayNumbersInPanel()
+        {
+            int lignes = 9;
+            int colonnes = 5;
+            int initial = 1;
+            int labellargeur = 40;
+            int labelhauteur = 20;
+            int espace = 1;
+
+            for (int i = 0; i < lignes; i++)
+            {
+                for (int j = 0; j < colonnes; j++)
+                {
+                    Label label = new Label();
+                    label.Text = initial.ToString();
+                    label.BorderStyle = BorderStyle.FixedSingle;
+                    label.Width = labellargeur;
+                    label.Height = labelhauteur;
+                    label.TextAlign = ContentAlignment.MiddleCenter;
+                    label.Location = new Point(j * (labellargeur + espace), i * (labelhauteur + espace));
+                    label.Click += Label_Click; // Un seul gestionnaire d'événement
+                    panel1.Controls.Add(label);
+                    initial++;
+
+                    if (initial > 42) break;
+                }
+            }
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label; // Conversion sécurisée
+            if (clickedLabel != null)              // Vérification
+            {
+                if (selectedLabels.Count < maxSelectedLabels)
+                {
+                    clickedLabel.BackColor = Color.Green;       // Action sur le Label
+                    selectedLabels.Add(clickedLabel);
+
+                    if (selectedLabels.Count == maxSelectedLabels)
+                    {
+                        //MessageBox.Show($"Vous avez sélectionné {maxSelectedLabels} labels.");
+
+                        label1.Text = "Chiffres Choisis";
+                        label1.Show();
+
+                        foreach (var item in selectedLabels)
+                        {
+                            l_chiffreChoisis.Text +=  item.Text + " - ";
+                        }
+
+                        l_chiffreChoisis.Show();
+
+                        foreach (Control control in panel1.Controls)
+                        {
+                            if (control is Label label && !selectedLabels.Contains(label))
+                            {
+                                label.Enabled = false;
+                            }
+                        }
+
+                        SelectionnerLesChiffresGagnants();
+                        ComparerLesChiffres();
+                    }
+                }
+            }
+        }
+
+        private void SelectionnerLesChiffresGagnants()
+        {
+            //1 Choisir un chiffre entre 1 et 42
+            //2 le mettre dans la liste
+
+            Random random = new Random();
+
+            int chiffreChoisi;
+
+            for (int i = 0; i < maxSelectedLabels; i++)
+            {
+                chiffreChoisi = random.Next(1, 43);
+                chiffresGagnants.Add(chiffreChoisi);
+            }
+
+            l_chiffreGagnants.Text = "Chiffre Gagnants: ";
+            l_chiffreGagnants.Show();
+
+            foreach (var item in chiffresGagnants)
+            {
+                l_listeDesChiffresGagnant.Text += item.ToString() + " - ";
+            }
+
+            l_listeDesChiffresGagnant.Show();
+        }
+
+        private void ComparerLesChiffres()
+        {
+            int chiffreQuiCorrespond = 0;
+
+            for (int i = 0; i < maxSelectedLabels; i++)
+            {
+                if(selectedLabels[i].Text == chiffresGagnants[i].ToString())
+                {
+                    chiffreQuiCorrespond++;
+                }
+            }            
+
+            if(chiffreQuiCorrespond == maxSelectedLabels)
+            {
+                MessageBox.Show("Super vous avez gagné le jackpot !!!");
+            }
+            else
+            {
+                MessageBox.Show("Dommage, vous avez perdu :( ");
+            }
+        }
+
+    }
+}
+
